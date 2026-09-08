87ccb448dd9d3729b141b6db9228dc014ce9b1e8
anhtu-pham <anhtupham99999@gmail.com>
2026-09-07 20:19:54 -0400

    Upload archive

--- L1DeepMETv2_SW/L1DeepMETv2/model/graph_met_network.py
+++ L1DeepMETv2_SW/L1DeepMETv2/model/graph_met_network.py
@@ -45,6 +45,7 @@ class GraphMETNetwork(nn.Module):
                                     nn.ELU(),
                                     nn.Linear(hidden_dim//2, output_dim)
                                    )
+
         if is_delphes:
             self.pdgs = [0, 6, 11, 13, 22, 45, 65]
         else:
